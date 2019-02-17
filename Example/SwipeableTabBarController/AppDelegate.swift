03d88c68cfb40aac9ac8820b4c85f3aa835fc01f
Marcos Griselli <marcosgriselli@gmail.com>
2019-02-17 12:54:24 -0300

    add support for danger and swiftlint

--- Example/SwipeableTabBarController/AppDelegate.swift
+++ Example/SwipeableTabBarController/AppDelegate.swift
@@ -13,10 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 }
-
